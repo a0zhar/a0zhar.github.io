5fbb09fb18ad5f2e5f16be79c1b6c3a1100ad1c2
a0zhar <96749372+a0zhar@users.noreply.github.com>
2023-08-26 09:23:08 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -9,36 +9,15 @@ function addScriptFile(_src, _id, _timeout) {
     document.head.appendChild(script);
   }, timeoutTime);
 }
-
-function executeBinFile(self) {
-  PLfile = self.getAttribute("binfile");
+function runSerotonin(self) {
+  
+  PLfile    = self.getAttribute("binfile");
   LoadedMSG = self.getAttribute("loadmsg");
   var xhr = new XMLHttpRequest();
   xhr.responseType = "arraybuffer";
   xhr.open("GET", PLfile, true);
   xhr.onload = function () {
     if (xhr.status === 200) {
-      let binSize = xhr.response.byteLength;
-      let binData = new Uint8Array(xhr.response);
-      let payload = new Uint32Array(binSize);
-      payload.set(binData);
-      window.pl_blob_len = "0x" + binSize.toString(16);
-      window.pl_blob = malloc(window.pl_blob_len);
-      write_mem(window.pl_blob, payload);
-      launch_payload();
-    }
-  };
-  xhr.send();
-}
-
-function runSerotonin(self) {
-  PLfile = self.getAttribute("binfile");
-  LoadedMSG = self.getAttribute("loadmsg");
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = "arraybuffer";
-  xhr.open("GET", PLfile, true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4) {
       var tmp0 = new Uint8Array(xhr.response.byteLength);
       tmp0.set(new Uint8Array(xhr.response), 0);
       var payload = new Uint32Array(tmp0);
@@ -46,8 +25,7 @@ function runSerotonin(self) {
       window.pl_blob_len = getlength;
       window.pl_blob = malloc(window.pl_blob_len);
       write_mem(window.pl_blob, payload);
-      addScriptFile("misc/netcat.js", "nett", 0);
-      addScriptFile("misc/pl_loader.js", "cccc", 0);
+      addScriptFile("misc/loader.js", "loader", 0);
     }
   };
   xhr.send();
